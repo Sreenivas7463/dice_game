f4b52469e7dfa119399753f6294a5f5fc2a64309
Pratham Malji <pmalji5034@gmail.com>
2019-10-04 21:01:02 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DICEE',
+      home:DiceApp()
+    ));
+
+class DiceApp extends StatefulWidget {
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+   var leftDiceNumber=1;
+    var rightDiceNumber=3;
+  void generateRandomNumber(){
+    setState(() {
+      leftDiceNumber=Random().nextInt(6)+1;
+      rightDiceNumber=Random().nextInt(6)+1;  
+    });
+  }
+
+   @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(title:Text('Dicee'), backgroundColor: Colors.red,),
+      backgroundColor: Colors.red,
+      body: Center(child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+              onPressed: (){print('button 1 pressed');
+              generateRandomNumber();
+               },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+           Expanded(
+              child: FlatButton(
+              onPressed: (){print('button 2 pressed');
+              generateRandomNumber();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),  ),
+    );
+  }
+}
